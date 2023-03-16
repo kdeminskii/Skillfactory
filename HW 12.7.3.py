bfd000c3d4b162f51d942511b3daab9e37522b51
kdeminskii <126471357+kdeminskii@users.noreply.github.com>
2023-03-17 01:46:25 +0300

    Add files via upload

---
+++ HW 12.7.3.py
@@ -0,0 +1,7 @@
+money = int(input('Введите сумму вклада: '))
+per_cent = {'ТКБ': 5.6, 'СКБ': 5.9, 'ВТБ': 4.28, 'СБЕР': 4.0}
+deposit = []
+for key in per_cent:
+    deposit.append(int(per_cent[key]*money/100))
+print(deposit)
+print("Максимальная сумма, которую вы можете заработать - " + str(max(deposit)) + " руб.")
